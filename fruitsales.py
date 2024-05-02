7d77ff58b62891c39f844ed0b9f5d855964eba6f
Walker Childers <ww.childers@att.net>
2024-05-02 18:30:39 -0400

    Added updated fruit sales code and fruits.csv file

--- fruitsales.py
+++ fruitsales.py
@@ -1,9 +1,9 @@
 # add your code here
 import pandas as pd
-fruit_sales = {
+fruit_sales = pd.DataFrame({
     'Apples' : [35,41],
     'Bananas' : [21,34]
-}
+}, index=['2017 Sales', '2018 Sales'])
+fruit_sales
+fruit_sales.to_csv("fruits.csv")
 
-new_fruit_sales = pd.DataFrame(fruit_sales, index=('2017 Sales', '2018 Sales'))
-print(new_fruit_sales)
