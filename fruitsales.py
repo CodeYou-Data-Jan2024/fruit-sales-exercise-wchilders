2d351a00bebadef6642388cb569b152ba4748eae
Walker Childers <ww.childers@att.net>
2024-05-01 21:24:20 -0400

    added my fruitsales code

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,9 @@
 # add your code here
+import pandas as pd
+fruit_sales = {
+    'Apples' : [35,41],
+    'Bananas' : [21,34]
+}
+
+new_fruit_sales = pd.DataFrame(fruit_sales, index=('2017 Sales', '2018 Sales'))
+print(new_fruit_sales)
